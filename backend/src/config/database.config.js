276c8d58812396d974033a58c1f77a074619d533
Himanshu Chuchra <chuchrahimanshu@gmail.com>
2024-01-28 09:33:10 +0530

    Setup Database Connection - MongoDB

---
+++ backend/src/config/database.config.js
@@ -0,0 +1,19 @@
+// TODO: Shift Database from Local MongoDB Compass to MongoDB Atlas Cloud
+
+import mongoose from "mongoose";
+
+(async () => {
+  try {
+    const database = await mongoose.connect(process.env.MONGODB_URI);
+    if (!database) {
+      console.log(`ERROR 💥 | MongoDB Connection | mongoose.config.js`);
+    }
+
+    const connectionString = database.connection._connectionString;
+    console.log(`SUCCESS 🚀 - MongoDB connected on ${connectionString}`);
+  } catch (error) {
+    console.log(`ERROR 💥 | MongoDB Connection | ${error}`);
+  }
+})();
+
+export default mongoose.connection;
