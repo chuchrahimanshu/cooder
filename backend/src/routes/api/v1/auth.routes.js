3992fe48226b66e6bf5d80a4740cd84ca24d4acc
Himanshu Chuchra <chuchrahimanshu@gmail.com>
2024-01-28 10:46:20 +0530

    Setup Routers for Version 1 of API

--- backend/src/routes/api/v1/auth.routes.js
+++ backend/src/routes/api/v1/auth.routes.js
@@ -1,12 +1,8 @@
 // Import Section
 import express from "express";
-import authRouter from "./auth.routes.js";
 
 // Configuration Section
 const router = express.Router();
-
-// Middleware Section
-router.use("/auth", authRouter);
 
 // Export Section
 export default router;
