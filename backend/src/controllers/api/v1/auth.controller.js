71710fe112262f8aec31e9888d1549fa45651205
Himanshu Chuchra <chuchrahimanshu@gmail.com>
2024-01-28 10:40:18 +0530

    Create Controllers Template for Auth Module

---
+++ backend/src/controllers/api/v1/auth.controller.js
@@ -0,0 +1,37 @@
+// Import Section
+import { asyncHandler } from "../../../utils/asyncHandler.util.js";
+
+// Controller Actions - End Points
+export const verifyNewUser = asyncHandler(async (req, res, next) => {});
+
+export const userSignUp = asyncHandler(async (req, res, next) => {});
+
+export const userSignIn = asyncHandler(async (req, res, next) => {});
+
+export const userSignOut = asyncHandler(async (req, res, next) => {});
+
+export const verifyUniqueUsername = asyncHandler(async (req, res, next) => {});
+
+export const generateChangePasswordToken = asyncHandler(
+  async (req, res, next) => {}
+);
+
+export const verifyTokenAndChangePassword = asyncHandler(
+  async (req, res, next) => {}
+);
+
+export const generateEmailVerificationToken = asyncHandler(
+  async (req, res, next) => {}
+);
+
+export const verifyEmailVerificationToken = asyncHandler(
+  async (req, res, next) => {}
+);
+
+export const generateTwoFactorVerificationToken = asyncHandler(
+  async (req, res, next) => {}
+);
+
+export const verifyTwoFactorVerification = asyncHandler(
+  async (req, res, next) => {}
+);
