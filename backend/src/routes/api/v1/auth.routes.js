404e7226df98b7c2f4ba7bf54f5bc6909617c75a
Himanshu Chuchra <iamhimanshuchuchra@gmail.com>
2024-01-28 10:59:42 +0530

    Create all Routes for Auth Module

--- backend/src/routes/api/v1/auth.routes.js
+++ backend/src/routes/api/v1/auth.routes.js
@@ -1,8 +1,36 @@
 // Import Section
 import express from "express";
+import {
+  generateChangePasswordToken,
+  generateEmailVerificationToken,
+  generateTwoFactorVerificationToken,
+  userSignIn,
+  userSignOut,
+  userSignUp,
+  verifyNewUser,
+  verifyUniqueUsername,
+  verifyTokenAndChangePassword,
+  verifyTwoFactorVerification,
+  verifyEmailVerificationToken,
+} from "../../../controllers/api/v1/auth.controller.js";
 
 // Configuration Section
 const router = express.Router();
+
+// Non - Authenticated Routes Section
+router.route("/").post(verifyNewUser);
+router.route("/sign-up").post(userSignUp);
+router.route("/sign-in").post(userSignIn);
+router.route("/verify/username").post(verifyUniqueUsername);
+router.route("/tokens/email").post(generateChangePasswordToken);
+router.route("/change-password").post(verifyTokenAndChangePassword);
+router.route("/tokens/tfa").post(generateTwoFactorVerificationToken);
+router.route("/verify/tfa").post(verifyTwoFactorVerification);
+
+// Authenticated Routes Section
+router.route("/sign-out").get(userSignOut);
+router.route("/tokens/email").get(generateEmailVerificationToken);
+router.route("/verify/email").post(verifyEmailVerificationToken);
 
 // Export Section
 export default router;
