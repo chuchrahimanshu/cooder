96b86a2b69599ce733577e91cd9dfebc89016113
Himanshu Chuchra <iamhimanshuchuchra@gmail.com>
2024-01-30 18:23:42 +0530

    Fix Auth Controllers Typo and Chaning

---
+++ backend/src/constants.js
@@ -0,0 +1,18 @@
+// AUTH Constants
+
+export const COOKIE_OPTIONS = {
+  httpOnly: true,
+  secure: true,
+};
+
+// EMAIL Constants
+
+export const SIGN_UP_EMAIL_SUBJECT = "Welcome to Codeial";
+export const CHANGE_PASSWORD_EMAIL_SUBJECT = "Change Password OTP";
+export const EMAIL_VERIFICATION_EMAIL_SUBJECT = "Email Verification OTP";
+export const TFA_EMAIL_SUBJECT = "Two Factor Authentication OTP";
+
+export const SIGN_UP_EMAIL_HBS = "testing";
+export const CHANGE_PASSWORD_EMAIL_HBS = "testing";
+export const EMAIL_VERIFICATION_EMAIL_HBS = "testing";
+export const TFA_EMAIL_HBS = "testing";
