3d6ebf55684529fedb6ebd616c26b8482fdea96d
Himanshu Chuchra <chuchrahimanshu@gmail.com>
2024-01-30 10:14:24 +0530

    Setup UserAgent as Middleware

---
+++ backend/src/middlewares/userAgent.middleware.js
@@ -0,0 +1,15 @@
+import UAParser from "ua-parser-js";
+
+const getUserAgent = (req, res, next) => {
+  const parser = UAParser(req.headers["user-agent"]);
+  const browserName = parser?.browser?.name;
+  const browserVersion = parser?.browser?.version;
+  const ua = parser.ua;
+  const userAgent =
+    browserName && browserVersion ? `${browserName}/${browserVersion}` : ua;
+
+  req.userAgent = { userAgent };
+  next();
+};
+
+export { getUserAgent };
