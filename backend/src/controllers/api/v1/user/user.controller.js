f84b3d1ae82e00855b5fcf28e70a0b6d5c23ce53
Himanshu Chuchra <chuchrahimanshu@gmail.com>
2024-01-31 08:39:41 +0530

    Add User Controller for Fetching User Details

--- backend/src/controllers/api/v1/user/user.controller.js
+++ backend/src/controllers/api/v1/user/user.controller.js
@@ -1,9 +1,35 @@
 // Import Section
+import { User } from "../../../../models/user/user.model.js";
 import { asyncHandler } from "../../../../utils/asyncHandler.util.js";
+import { APIError } from "../../../../utils/errorHandler.util.js";
+import { APIResponse } from "../../../../utils/responseHandler.util.js";
 
 // Controller Actions - End Points
 
-export const getAllUsers = asyncHandler(async (req, res, next) => {});
-export const getSingleUser = asyncHandler(async (req, res, next) => {});
+export const getAllUsers = asyncHandler(async (req, res, next) => {
+  const users = await User.find();
+
+  return res.status(200).json(
+    new APIResponse(200, "All users fetched successfully", {
+      size: users.length,
+      users,
+    })
+  );
+});
+
+export const getSingleUser = asyncHandler(async (req, res, next) => {
+  const user = await User.findById(req.user?._id);
+  if (!user) {
+    return res.status(401).json(new APIError(401, "Unauthorized Access"));
+  }
+
+  return res.status(200).json(
+    new APIResponse(200, "User details fetched successfully", {
+      user,
+    })
+  );
+});
+
 export const updateUser = asyncHandler(async (req, res, next) => {});
+
 export const deleteUser = asyncHandler(async (req, res, next) => {});
---
+++ backend/src/middlewares/user.middleware.js
@@ -0,0 +1,13 @@
+import { APIError } from "../utils/errorHandler.util";
+
+const verifyUser = async (req, res, next) => {
+  const { id } = req.params;
+
+  if (req.user._id.toString() !== id.toString()) {
+    return res.status(401).json(new APIError(401, "Unauthorized Access"));
+  }
+
+  next();
+};
+
+export { verifyUser };
