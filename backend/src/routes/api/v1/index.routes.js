cc8db4d71339337b74018039ff532d4fbb951896
Himanshu Chuchra <iamhimanshuchuchra@gmail.com>
2024-01-31 08:23:47 +0530

    Setup User Routes and Controller End Points

--- backend/src/routes/api/v1/index.routes.js
+++ backend/src/routes/api/v1/index.routes.js
@@ -1,12 +1,14 @@
 // Import Section
 import express from "express";
 import authRouter from "./auth/auth.routes.js";
+import userRouter from "./user/user.routes.js";
 
 // Configuration Section
 const router = express.Router();
 
 // Middleware Section
 router.use("/auth", authRouter);
+router.use("/user", userRouter);
 
 // Export Section
 export default router;
