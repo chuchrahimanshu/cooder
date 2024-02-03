0d293ecfde76efee90057be740b7a4db85ec827f
Himanshu Chuchra <iamhimanshuchuchra@gmail.com>
2024-02-03 10:10:41 +0530

    Create Task Model under Tasks Module

---
+++ backend/src/models/task/task.model.js
@@ -0,0 +1,37 @@
+// Import Section
+import mongoose from "mongoose";
+
+// Schema Section
+const taskSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+    },
+    deadline: {
+      type: Date,
+    },
+    status: {
+      type: String,
+      enum: ["Not Started", "In Progress", "Completed"],
+      default: "Not Started",
+    },
+    tags: [
+      {
+        type: String,
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  }
+);
+
+// Mongoose Model
+const Task = mongoose.model("Task", taskSchema);
+
+// Export Section
+export { Task };
