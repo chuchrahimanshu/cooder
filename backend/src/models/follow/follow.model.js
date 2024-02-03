6d13ddf7705d25b2f3b1016ccd64a8860186cfea
Himanshu Chuchra <iamhimanshuchuchra@gmail.com>
2024-02-03 09:54:51 +0530

    Create Follow Model under Follows Module

---
+++ backend/src/models/follow/follow.model.js
@@ -0,0 +1,27 @@
+// Import Section
+import mongoose from "mongoose";
+
+// Schema Section
+const followSchema = new mongoose.Schema(
+  {
+    follower: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    following: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+// Mongoose Model
+const Follow = mongoose.model("Follow", followSchema);
+
+// Export Section
+export { Follow };
