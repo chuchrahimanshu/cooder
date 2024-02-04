524395bee4309ad2f6c43dfdd205e4c5a325356d
Himanshu Chuchra <chuchrahimanshu@gmail.com>
2024-02-04 14:03:23 +0530

    Create Routes of Ticket and Solution under Debugger Model

---
+++ backend/src/routes/api/v1/user/debugger/ticket.routes.js
@@ -0,0 +1,20 @@
+// Import Section
+import express from "express";
+import solutionRouter from "./solution.routes.js";
+
+// Configuration Section
+const router = express.Router({ mergeParams: true });
+
+// Middleware Section
+router.use("/:ticketid/solutions", solutionRouter);
+
+// Authenticated Routes Section
+router.route("/").get(getAllTickets).post(createTicket);
+router
+  .route("/:ticketid")
+  .get(getSingleTicket)
+  .patch(updateTicket)
+  .delete(deleteTicket);
+router.route("/:ticketid/rating").post(addTicketRating);
+// Export Section
+export default router;
