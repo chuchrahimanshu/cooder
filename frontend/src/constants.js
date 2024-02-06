a1b0efeb25db5c60579732b8034d05aacf633531
Himanshu Chuchra <chuchrahimanshu@gmail.com>
2024-02-06 11:01:22 +0530

    Create Banner Component for Auth Module

---
+++ frontend/src/constants.js
@@ -0,0 +1,7 @@
+// AUTH
+
+export const BANNER_TEXT_AUTHENTICATE = "👩‍💻 Welcome, Coding Maestro! 👨‍💻";
+export const BANNER_TEXT_SIGN_UP = "👩‍💻 Welcome, Coding Maestro! 👨‍💻";
+export const BANNER_TEXT_SIGN_IN = "👾 real developers count from 0 🧮";
+export const BANNER_TEXT_TFA = "🔐 OTP - One Tap, Programmer 👨‍💻";
+export const BANNER_TEXT_CHANGE_PASSWORD = "🔑 Devs Forget, Wizards Reset! 🧙‍♂️";
--- frontend/src/pages/auth/Authenticate.jsx
+++ frontend/src/pages/auth/Authenticate.jsx
@@ -1,7 +1,13 @@
 import React from "react";
+import { Banner } from "../../components/auth/Banner";
+import { BANNER_TEXT_AUTHENTICATE } from "../../constants";
 
 const Authenticate = () => {
-  return <div>Authenticate</div>;
+  return (
+    <div>
+      <Banner message={BANNER_TEXT_AUTHENTICATE} />
+    </div>
+  );
 };
 
 export { Authenticate };
