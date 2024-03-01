b7702aa48937b4c792cc1a86875ae0c350a0b191
Himanshu Chuchra <chuchrahimanshu@gmail.com>
2024-03-01 12:45:47 +0530

    Design Post Content and Footer - Social Module

--- frontend/src/components/social/post/Post.jsx
+++ frontend/src/components/social/post/Post.jsx
@@ -1,6 +1,13 @@
 import React from "react";
 import { useSelector } from "react-redux";
 import { HiDotsHorizontal } from "react-icons/hi";
+import {
+  TiHeartFullOutline,
+  TiUserAdd,
+  TiArrowRepeat,
+  TiEdit,
+} from "react-icons/ti";
+import { FaCommentDots } from "react-icons/fa6";
 
 const Post = () => {
   // Hooks Configuration
@@ -20,6 +27,35 @@ const Post = () => {
           </div>
         </div>
         <HiDotsHorizontal className="post__header-options" />
+      </section>
+      <section className="post__body">
+        <p className="post__body-text">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo
+          explicabo, quas natus voluptatem velit ab optio recusandae debitis
+          fuga animi architecto error omnis ipsum officiis incidunt consequatur
+          nihil itaque maxime labore. Velit, placeat, quisquam, officiis veniam
+          ullam explicabo repellendus similique inventore dolores laboriosam
+          cumque obcaecati et totam vero numquam facere labore nemo beatae. Quod
+          facere aliquam sed eligendi.
+        </p>
+      </section>
+      <section className="post__footer">
+        <section className="post__footer-section" id="post__like">
+          <TiHeartFullOutline className="post__footer-section-icons" />
+          <p className="post__footer-section-text">Like</p>
+        </section>
+        <section className="post__footer-section" id="post__comment">
+          <TiEdit className="post__footer-section-icons" />
+          <p className="post__footer-section-text">Comment</p>
+        </section>
+        <section className="post__footer-section" id="post__repost">
+          <TiArrowRepeat className="post__footer-section-icons" />
+          <p className="post__footer-section-text">Repost</p>
+        </section>
+        <section className="post__footer-section" id="post__follow">
+          <TiUserAdd className="post__footer-section-icons" />
+          <p className="post__footer-section-text">Follow</p>
+        </section>
       </section>
     </div>
   );
