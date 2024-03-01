7a70dfa930fa75117d1cbb6777bef129c7c839b7
Himanshu Chuchra <chuchrahimanshu@gmail.com>
2024-03-01 10:53:55 +0530

    Design Home Menu Section - Global Module

--- frontend/src/components/home/HomeMenu.jsx
+++ frontend/src/components/home/HomeMenu.jsx
@@ -1,5 +1,18 @@
+// Import Section
 import React from "react";
 import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+
+// Import Utilities
+import {
+  FcHome,
+  FcShop,
+  FcSettings,
+  FcTodoList,
+  FcPlanner,
+  FcReading,
+} from "react-icons/fc";
+import { IoBug } from "react-icons/io5";
 
 const HomeMenu = () => {
   // Hooks Configuration
@@ -10,6 +23,36 @@ const HomeMenu = () => {
     <div className="home__menu">
       <img src={user?.cover} alt="User Cover" className="home__menu-cover" />
       <img src={user?.avatar} alt="User Avatar" className="home__menu-avatar" />
+      <p className="home__menu-text-primary">{`${user?.firstName} ${user?.lastName}`}</p>
+      <p className="home__menu-text-secondary">@{user?.username}</p>
+      <Link className="home__menu-links">
+        <FcHome className="home__menu-links--icon" />
+        <p className="home__menu-links--text">Feed</p>
+      </Link>
+      <Link className="home__menu-links">
+        <FcTodoList className="home__menu-links--icon" />
+        <p className="home__menu-links--text">Todo List</p>
+      </Link>
+      <Link className="home__menu-links">
+        <FcShop className="home__menu-links--icon" />
+        <p className="home__menu-links--text">Marketplace</p>
+      </Link>
+      <Link className="home__menu-links">
+        <IoBug className="home__menu-links--icon text-red" />
+        <p className="home__menu-links--text">Bug Solver</p>
+      </Link>
+      <Link className="home__menu-links">
+        <FcReading className="home__menu-links--icon" />
+        <p className="home__menu-links--text">Learning</p>
+      </Link>
+      <Link className="home__menu-links">
+        <FcPlanner className="home__menu-links--icon" />
+        <p className="home__menu-links--text">Events</p>
+      </Link>
+      <Link className="home__menu-links">
+        <FcSettings className="home__menu-links--icon" />
+        <p className="home__menu-links--text">Settings</p>
+      </Link>
     </div>
   );
 };
