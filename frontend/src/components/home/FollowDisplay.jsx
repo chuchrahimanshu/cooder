1c3d6dbdf3a57458c6c97d847b366496f516a4d7
Himanshu Chuchra <iamhimanshuchuchra@gmail.com>
2024-03-09 12:23:07 +0530

    Setup API Call for Follow Relation

--- frontend/src/components/home/FollowDisplay.jsx
+++ frontend/src/components/home/FollowDisplay.jsx
@@ -2,11 +2,13 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getAllUsers } from "../../redux/user/user.slice";
+import { updateFollowRelation } from "../../redux/follow/followSlice";
 
 const FollowDisplay = () => {
   // Hooks Configuration
   const dispatch = useDispatch();
   const { users } = useSelector((state) => state.user);
+  const { user } = useSelector((state) => state.auth);
 
   useEffect(() => {
     if (!users) {
@@ -19,24 +21,31 @@ const FollowDisplay = () => {
     <>
       {users && (
         <ul className="follow-display">
-          {users.map((user) => (
-            <li className="follow-display__list" key={user._id}>
+          {users.map((element) => (
+            <li className="follow-display__list" key={element._id}>
               <section className="follow-display__list-user">
                 <img
-                  src={user.avatar}
+                  src={element.avatar}
                   alt="User Avatar"
                   className="follow-display__list-user-avatar"
                 />
                 <section className="follow-display__list-user-details">
                   <p className="follow-display__list-user-username">
-                    {user.username}
+                    {element.username}
                   </p>
-                  <p className="follow-display__list-user-name">{`${user.firstName} ${user.lastName}`}</p>
+                  <p className="follow-display__list-user-name">{`${element.firstName} ${element.lastName}`}</p>
                 </section>
               </section>
               <button
                 className="follow-display__list-button"
-                onClick={() => {}}>
+                onClick={() => {
+                  dispatch(
+                    updateFollowRelation({
+                      userid: user._id,
+                      followid: element._id,
+                    })
+                  );
+                }}>
                 Follow
               </button>
             </li>
