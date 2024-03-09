293ecdff78bdd1ec6b5e22fc9fd8e57efaa6d598
Himanshu Chuchra <iamhimanshuchuchra@gmail.com>
2024-03-09 08:35:48 +0530

    Add Controller Action for Follow Module

--- backend/src/controllers/api/v1/user/follow/follow.controller.js
+++ backend/src/controllers/api/v1/user/follow/follow.controller.js
@@ -1,5 +1,55 @@
 // Import Section
+import { Follow } from "../../../../../models/follow/follow.model.js";
 import { asyncHandler } from "../../../../../utils/asyncHandler.util.js";
+import { APIResponse } from "../../../../../utils/index.js";
+import mongoose from "mongoose";
 
 // Controller Actions - End Points
-export const makeFollower = asyncHandler(async (req, res, next) => {});
+export const makeFollower = asyncHandler(async (req, res, next) => {
+  const { userid, followid } = req.params;
+
+  if (userid === followid) {
+    return res
+      .status(200)
+      .json(new APIResponse(200, "User id and follow id cannot be same"));
+  }
+
+  const alreadyFollower = await Follow.aggregate([
+    {
+      $match: {
+        $and: [
+          {
+            follower: {
+              $eq: new mongoose.Types.ObjectId(userid),
+            },
+          },
+          {
+            following: {
+              $eq: new mongoose.Types.ObjectId(followid),
+            },
+          },
+        ],
+      },
+    },
+  ]);
+
+  if (alreadyFollower?.length > 0) {
+    await Follow.findByIdAndDelete(alreadyFollower[0]._id);
+    return res.status(200).json(
+      new APIResponse(200, "User unfollowed successfully", {
+        alreadyFollower,
+      })
+    );
+  }
+
+  await Follow.create({
+    follower: userid,
+    following: followid,
+  });
+
+  return res.status(200).json(
+    new APIResponse(200, "User followed successfully", {
+      alreadyFollower,
+    })
+  );
+});
